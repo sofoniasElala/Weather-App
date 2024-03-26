8efde9604e1bf48ba78e18eebba441f7fc1246e4
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-03-26 00:47:14 -0400

    add weather icons and font

---
+++ src/iconsToImport.js
@@ -0,0 +1,26 @@
+import icon1 from "./assets/icons/1.svg";
+import icon3 from "./assets/icons/3.svg";
+import icon5 from "./assets/icons/5.svg";
+import icon6 from "./assets/icons/6.svg";
+import icon8 from "./assets/icons/8.svg";
+import icon11 from "./assets/icons/11.svg";
+import icon12 from "./assets/icons/12.svg";
+import icon13 from "./assets/icons/13.svg";
+import icon15 from "./assets/icons/15.svg";
+import icon16 from "./assets/icons/16.svg";
+import icon18 from "./assets/icons/18.svg";
+import icon20 from "./assets/icons/20.svg";
+import icon22 from "./assets/icons/22.svg";
+import icon23 from "./assets/icons/23.svg";
+import icon24 from "./assets/icons/24.svg";
+import icon32 from "./assets/icons/32.svg";
+import icon33 from "./assets/icons/33.svg";
+import icon37 from "./assets/icons/37.svg";
+import icon38 from "./assets/icons/38.svg";
+import icon39 from "./assets/icons/39.svg";
+import icon41 from "./assets/icons/41.svg";
+import rise from "./assets/icons/rise.svg";
+import set from "./assets/icons/set.svg";
+
+export { icon1, icon3, icon5, icon6, icon8, icon11, icon12, icon13, icon15, icon16, icon18, icon20,
+icon22, icon23, icon24, icon32, icon33, icon37, icon38, icon39, icon41, rise, set };
